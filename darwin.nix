80e3a498c0fa08bf1341789ec505ee9c92b74d31
pmihaly <misi@pappmihaly.com>
2023-02-04 17:37:23 +0100

    add mpv

--- darwin.nix
+++ darwin.nix
@@ -78,6 +78,43 @@
       enableAliases = true;
     };
 
+    programs.mpv = {
+      enable = true;
+      config = {
+        fullscreen = true;
+        ytdl-format = "bestvideo[height<=?1080][vcodec!=?vp9]+bestaudio/best";
+        osc = false;
+      };
+      defaultProfiles = [ "gpu-hq" ];
+      scripts = [
+        pkgs.mpvScripts.sponsorblock 
+        pkgs.mpvScripts.thumbnail
+      ];
+      bindings = {
+        "l" = "seek 5";
+        "h" = "seek -5";
+        "j" = "seek -60";
+        "k" = "seek 60";
+
+        "s" = "cycle sub";
+        "a" = "cycle audio";
+
+        "Alt+h" = "add chapter -1";
+        "Alt+l" = "add chapter 1";
+        "Ctrl+SPACE" = "add chapter 1";
+
+        "Alt+j" = "add video-zoom -0.25";
+        "Alt+k" = "add video-zoom 0.25";
+
+        "Alt+J" = "add sub-pos -1";
+        "Alt+K" = "add sub-pos +1";
+
+        "Ctrl+h" = "multiply speed 1/1.1";
+        "Ctrl+l" = "multiply speed 1.1";
+        "Ctrl+H" = "set speed 1.0";
+      };
+    };
+
     programs.lf = {
       enable = true;
       settings = {
