242df25de86d36e41847b69ee5fb36250ad01598
pmihaly <misi@pappmihaly.com>
2023-02-18 18:46:47 +0100

    make floaterm fit the terminal

--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -8,9 +8,9 @@ return {
       { '<leader>v', function() vim.api.nvim_command('FloatermNew --autoclose=2 lazygit') end, mode = 'n'},
       { '<leader>f', function() vim.api.nvim_command('FloatermNew --autoclose=2 lf') end, mode = 'n'},
     },
-    init = function()
+    config = function()
       vim.g.floaterm_width = 150
-      vim.g.floaterm_height = 50
+      vim.g.floaterm_height = 40
       vim.g.floaterm_title = false
       vim.g.floaterm_opener = 'edit'
     end
