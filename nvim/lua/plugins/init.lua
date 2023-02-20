a0f06d93f2a4c0f7bf3bef589ac5887d2d88ac9a
pmihaly <misi@pappmihaly.com>
2023-02-20 17:31:05 +0100

    display markdown previews in light mode

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -39,6 +39,9 @@ return {
     'iamcco/markdown-preview.nvim',
     ft = { "markdown" },
     build = 'cd app && npm install',
-    init = function() vim.g.mkdp_filetypes = { "markdown" } end
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_theme = 'light'
+    end
   }
 }
