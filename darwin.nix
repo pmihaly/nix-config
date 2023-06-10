827bd96c54dafa9d2de33cecd29e3b20b20b6de3
pmihaly <misi@pappmihaly.com>
2023-06-10 19:51:07 +0200

    add custom iosevka font

--- darwin.nix
+++ darwin.nix
@@ -286,7 +286,7 @@
     programs.kitty = {
       enable = true;
       font = {
-        name = "FiraCode Nerd Font Mono";
+        name = "Iosevka Custom Medium Extended";
         size = 13;
       };
       theme = if darkMode then "Nord" else null;
@@ -451,8 +451,45 @@
   fonts = {
     fontDir.enable = true;
     fonts = [
-      pkgs.nerdfonts
-    ];
+      (pkgs.nerdfonts.override {
+        fonts = ["FiraCode"];
+      })
+      (pkgs.iosevka.override {
+        privateBuildPlan = ''
+          [buildPlans.iosevka-custom]
+          family = "Iosevka Custom"
+          spacing = "fontconfig-mono"
+          serifs = "slab"
+          no-cv-ss = true
+          export-glyph-names = true
+
+            [buildPlans.iosevka-custom.ligations]
+            inherits = "dlig"
+
+          [buildPlans.iosevka-custom.weights.regular]
+          shape = 400
+          menu = 400
+          css = 400
+
+          [buildPlans.iosevka-custom.weights.medium]
+          shape = 500
+          menu = 500
+          css = 500
+
+          [buildPlans.iosevka-custom.weights.semibold]
+          shape = 600
+          menu = 600
+          css = 600
+
+          [buildPlans.iosevka-custom.slopes.upright]
+          angle = 0
+          shape = "upright"
+          menu = "upright"
+          css = "normal"
+        '';
+        set = "custom";
+      })
+      ];
   };
 
   services.yabai = {
