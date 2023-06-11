cde1ab1d70302218a43fd51213d1c6412495838c
pmihaly <misi@pappmihaly.com>
2023-06-11 13:57:03 +0200

    replace walters prompt with igloo

--- darwin.nix
+++ darwin.nix
@@ -221,21 +221,38 @@
 
       programs.zsh = {
         enable = true;
-        initExtra = ''
-          autoload -U promptinit; promptinit
-          prompt walters
+        initExtra = builtins.concatStringsSep "\n"
+          [
+            ''
+              autoload -U promptinit; promptinit
+              # prompt walters
 
-          set -o vi
+              set -o vi
 
-          zstyle ':completion:*:*:*:default' menu yes select search interactive # browseable, searchable completions
-          zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+              # zstyle ':completion:*:*:*:default' menu yes select search interactive # browseable, searchable completions
+              zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 
-          autoload -z edit-command-line
-          zle -N edit-command-line
-          bindkey "^X" edit-command-line
+              autoload -z edit-command-line
+              zle -N edit-command-line
+              bindkey "^X" edit-command-line
 
-          [ -f ~/.zshrc_work ] && . ~/.zshrc_work
-        '';
+              export IGLOO_ZSH_PROMPT_THEME_HIDE_TIME=true
+
+              [ -f ~/.zshrc_work ] && . ~/.zshrc_work
+            ''
+            (builtins.readFile
+              (builtins.fetchurl
+                {
+                  url = "https://raw.githubusercontent.com/git/git/4ca12e10e6fbda68adcb32e78497dc261e94734d/contrib/completion/git-prompt.sh";
+                  sha256 = "0rjwxf1vfkynjqns6drhbrfv7358zyhhx5j4zgawm25qwza4xggi";
+                }))
+            (builtins.readFile
+              (builtins.fetchurl
+                {
+                  url = "https://raw.githubusercontent.com/arcticicestudio/igloo/18b735009f2baa29e3bbe1323a1fc2082f88b393/snowblocks/zsh/lib/themes/igloo.zsh";
+                  sha256 = "17ln78ww66mg1k2yljpap74rf6fjjiw818x4pc2d5l6yjqgv8wfl";
+                }))
+          ];
         enableCompletion = true;
         completionInit = ''
           autoload bashcompinit && bashcompinit
