d79d2b049caecc55e1b6e028d92700acfc1935e7
pmihaly <misi@pappmihaly.com>
2023-06-12 21:06:46 +0200

    disable italics in nvim

--- nvim/lua/plugins/visuals.lua
+++ nvim/lua/plugins/visuals.lua
@@ -5,6 +5,7 @@ return {
     priority = 1000,
     config = function()
       vim.g.nord_disable_background = true;
+      vim.g.nord_italic = false;
       vim.cmd([[colorscheme nord]])
     end,
   },
