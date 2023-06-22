74bffa1f8b62e6184536139b701988c7f70e7786
pmihaly <misi@pappmihaly.com>
2023-06-22 07:20:06 +0200

    remove remnants of x11

--- nixos.nix
+++ nixos.nix
@@ -61,9 +61,6 @@ in
     pkgs.iosevka-custom
   ];
 
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "misi";
-
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
