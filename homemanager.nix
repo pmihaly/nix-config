807d11e6b0ca1b78676043e982d541f2f7f291ab
pmihaly <misi@pappmihaly.com>
2023-06-27 16:57:00 +0200

    remvoe underline fix

--- homemanager.nix
+++ homemanager.nix
@@ -345,10 +345,6 @@
           confirm_os_window_close = 0;
           close_on_child_death = true;
         };
-        extraConfig = ''
-              modify_font underline_position 0.5
-              modify_font underline_thickness 130%
-            '';
       };
 
       programs.btop = {
