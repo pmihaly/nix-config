4a007a08d20fbfcc900251c7bf37e4162e272ead
Mihaly Papp <misi@pappmihaly.com>
2023-07-10 06:12:34 +0200

    set nordzly cursor

--- nixos.nix
+++ nixos.nix
@@ -287,6 +287,10 @@ in
             package = pkgs.nordzy-icon-theme;
             name = "Nordzy";
           };
+          cursorTheme = {
+            package = pkgs.nordzy-cursor-theme;
+            name = "Nordzy-cursors";
+          };
         };
       }
     ];
