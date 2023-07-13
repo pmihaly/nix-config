53599faf1ec96a093c0c591820487d61967f7797
pmihaly <misi@pappmihaly.com>
2023-07-13 12:58:46 +0200

    add some other vscode settings

--- homemanager.nix
+++ homemanager.nix
@@ -431,11 +431,30 @@
       arcticicestudio.nord-visual-studio-code
       eamodio.gitlens
       asvetliakov.vscode-neovim
+      esbenp.prettier-vscode
+      dbaeumer.vscode-eslint
     ];
     userSettings = {
       "workbench.colorTheme" = "Nord";
       "editor.cursorSmoothCaretAnimation" = "on";
       "editor.smoothScrolling" = true;
+      "editor.minimap.enabled" = false;
+      "editor.formatOnSave" = true;
+      "editor.cursorSurroundingLines" = 999;
+      "editor.cursorSurroundingLinesStyle" = "all";
+      "files.autoSave" = "onFocusChange";
+      "files.insertFinalNewline" = true;
+      "files.trimTrailingWhitespace" = true;
+      "explorer.autoReveal" = true;
+      "workbench.editor.enablePreview" = false;
+      "workbench.editor.tabCloseButton" = "right";
+      "workbench.editor.tabSizing" = "shrink";
+      "workbench.panel.defaultLocation" = "right";
+      "workbench.settings.editor" = "json";
+      "workbench.sideBar.location" = "right";
+      "[javascript]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
     };
   };
 }
