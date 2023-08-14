0e95d588a3d7490205fe17da844334c164721cfe
pmihaly <misi@pappmihaly.com>
2023-08-14 19:17:02 +0200

    map dr to restart dk to openballoon

--- nvim/lua/plugins/vimspector.lua
+++ nvim/lua/plugins/vimspector.lua
@@ -6,12 +6,12 @@ return {
       { '<Leader>dc', ':call vimspector#Continue()<CR>', mode = 'n' },
       { '<Leader>dt', ':call vimspector#ToggleBreakpoint()<CR>', mode = 'n' },
       { '<Leader>dT', ':call vimspector#ClearBreakpoints()<CR>', mode = 'n' },
-      { '<Leader>dk', '<Plug>VimspectorRestart', mode = 'n' },
+      { '<Leader>dr', '<Plug>VimspectorRestart', mode = 'n' },
       { '<Leader>dh', '<Plug>VimspectorStepOut', mode = 'n' },
       { '<Leader>dl', '<Plug>VimspectorStepInto', mode = 'n' },
       { '<Leader>dj', '<Plug>VimspectorRunToCursor', mode = 'n' },
-      { '<Leader>di', '<Plug>VimspectorBalloonEval', mode = 'n' },
-      { '<Leader>di', '<Plug>VimspectorBalloonEval', mode = 'x' },
+      { '<Leader>dk', '<Plug>VimspectorBalloonEval', mode = 'n' },
+      { '<Leader>dk', '<Plug>VimspectorBalloonEval', mode = 'x' },
     },
     config = function()
       vim.g.vimspector_base_dir = '/Users/mihaly.papp/.local/share/nvim/lazy/vimspector'
