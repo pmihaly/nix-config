50e7831c7f833df27d42ac20e0f92d0c8415017e
pmihaly <misi@pappmihaly.com>
2023-08-18 13:24:03 +0200

    add tab completion

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -16,7 +16,7 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      {'L3MON4D3/LuaSnip'},
+      -- {'L3MON4D3/LuaSnip'},
     },
     config = function()
       -- Here is where you configure the autocompletion settings.
@@ -31,9 +31,19 @@ return {
 
       cmp.setup({
         mapping = {
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+            if cmp.visible() then
+              local entry = cmp.get_selected_entry()
+              if not entry then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+              else
+                cmp.confirm()
+              end
+            else
+              fallback()
+            end
+          end, {"i","s","c",}),
         }
       })
     end
