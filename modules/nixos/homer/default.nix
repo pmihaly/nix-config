9dc9bf5fa429ebda47ffe675db22103f6c68aeb2
pmihaly <misi@pappmihaly.com>
2023-11-19 17:59:05 +0100

    add catppuccin homer theme

--- modules/nixos/homer/default.nix
+++ modules/nixos/homer/default.nix
@@ -7,10 +7,7 @@ in {
   options.modules.homer = {
     enable = mkEnableOption "homer";
     homerConfig = mkOption {
-      default = {
-        title = "❄️🧙";
-        logo = "assets/logo.png";
-      };
+      default = { };
       type = types.attrs;
     };
     services = mkOption {
@@ -43,12 +40,46 @@ in {
             }) groupData;
           }) cfg.services;
 
-          homerConfig = { services = mappedServices; } // cfg.homerConfig;
+          homerConfig = {
+            documentTitle = "❄️🧙";
+            title = "❄️🧙";
+            subtitle = "skylake";
+            logo = "assets/dark_circle.png";
+            stylesheet = [ "assets/catppuccin-mocha.css" ];
+            services = mappedServices;
+            footer = false;
+          } // cfg.homerConfig;
+
+          style = builtins.fetchurl {
+            url =
+              "https://raw.githubusercontent.com/mrpbennett/catppuccin-homer/main/flavours/catppuccin-mocha.css";
+            sha256 = "1jkj55fqv1wyx71idfbwyfjy66r4hmfxlk1lpxqxhnjwizi037xq";
+          };
+          background = builtins.fetchurl {
+            url =
+              "https://raw.githubusercontent.com/mrpbennett/catppuccin-homer/main/assets/images/backgrounds/romb.png";
+            sha256 = "14yaxr50db4h93cc1i9qhr5g2r33dds64p2yl7pz4w01xxp2qmh7";
+          };
+          logo = builtins.fetchurl {
+            url =
+              "https://raw.githubusercontent.com/mrpbennett/catppuccin-homer/main/assets/logos/dark_circle.png";
+            sha256 = "1amnsl2vs5hc0jklmpn69lyvrnkckz8v9c9r70hdjjkns82p1kh3";
+          };
+          favicons = pkgs.fetchzip {
+            url =
+              "https://raw.githubusercontent.com/mrpbennett/catppuccin-homer/main/assets/favicons/dark_favicon.zip";
+            hash = "sha256-xp9A4sesO1O0u5vwnFSQrhN+PTqo/v0iiHvarwdoPi8";
+            stripRoot = false;
+          };
+
         in {
           image = "b4bz/homer:v23.10.1";
           ports = [ "8080:8080" ];
           volumes = [
-            "${./assets}:/www/assets"
+            "${style}:/www/assets/catppuccin-mocha.css"
+            "${background}:/www/assets/images/romb.png"
+            "${logo}:/www/assets/dark_circle.png"
+            "${favicons}:/www/assets/icons"
             "${
               (pkgs.formats.yaml { }).generate "config.yml" homerConfig
             }:/www/assets/config.yml"
