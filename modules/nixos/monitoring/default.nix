ccad208b2e0f8708b563a2024b795f3b53bb2ca9
pmihaly <misi@pappmihaly.com>
2023-11-19 10:41:11 +0100

    add prometheus

--- modules/nixos/monitoring/default.nix
+++ modules/nixos/monitoring/default.nix
@@ -7,6 +7,30 @@ in {
   options.modules.monitoring = { enable = mkEnableOption "monitoring"; };
   config = mkIf cfg.enable (mkMerge [
 
+    {
+      services.prometheus = {
+        enable = true;
+        port = 9001;
+        exporters = {
+          node = {
+            enable = true;
+            enabledCollectors = [ "systemd" ];
+            port = 9002;
+          };
+        };
+        scrapeConfigs = [{
+          job_name = "skylake";
+          static_configs = [{
+            targets = [
+              "127.0.0.1:${
+                toString config.services.prometheus.exporters.node.port
+              }"
+            ];
+          }];
+        }];
+      };
+    }
+
     (mkService {
       subdomain = "grafana";
       port = 2342;
@@ -26,6 +50,14 @@ in {
             enforce_domain = true;
           };
           users = { allow_sign_up = false; };
+        };
+        provision.datasources.settings = {
+          apiVersion = 1;
+          datasources = [{
+            name = "Prometheus";
+            url = "http://localhost:9001";
+            type = "prometheus";
+          }];
         };
       };
     })
