2a09e3d39bd73f406f14f47dbecd0567a8e67557
pmihaly <misi@pappmihaly.com>
2023-11-22 09:31:18 +0100

    fix vimspector <leader>dd loading

--- modules/nvim/nvim/lua/plugins/vimspector.lua
+++ modules/nvim/nvim/lua/plugins/vimspector.lua
@@ -20,7 +20,7 @@ return {
   },
   {
     'nvim-telescope/telescope-vimspector.nvim',
-    depends = { 'puremourning/vimspector' },
+    dependencies = { 'puremourning/vimspector' },
     build = function() pcall(require('telescope').load_extension, 'vimspector') end,
     keys = {
       { '<leader>dd', '<cmd>Telescope vimspector configurations<cr>', mode = 'n'},
