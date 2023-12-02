b33cd30c741cad6d1e518549f17ee3dcce0a2010
pmihaly <misi@pappmihaly.com>
2023-12-02 15:43:16 +0100

    update nvim plugins, especially harpoon

--- modules/nvim/nvim/lua/plugins/harpoon.lua
+++ modules/nvim/nvim/lua/plugins/harpoon.lua
@@ -2,17 +2,22 @@ return {
   {
     'ThePrimeagen/harpoon',
     event = 'VeryLazy',
+    branch = 'harpoon2',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    },
     init = function()
-      local mark = require('harpoon.mark')
-      local ui = require('harpoon.ui')
+      local harpoon = require('harpoon')
 
-      vim.keymap.set('n', '<leader>n', mark.add_file)
-      vim.keymap.set('n', '<leader>i', ui.toggle_quick_menu)
+      harpoon:setup()
 
-      vim.keymap.set('n', '<leader>h', function() ui.nav_file(1) end)
-      vim.keymap.set('n', '<leader>j', function() ui.nav_file(2) end)
-      vim.keymap.set('n', '<leader>k', function() ui.nav_file(3) end)
-      vim.keymap.set('n', '<leader>l', function() ui.nav_file(4) end)
+      vim.keymap.set('n', '<leader>n', function() harpoon:list():append() end)
+      vim.keymap.set('n', '<leader>i', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set('n', '<leader>h', function() harpoon:list():select(1) end)
+      vim.keymap.set('n', '<leader>j', function() harpoon:list():select(2) end)
+      vim.keymap.set('n', '<leader>k', function() harpoon:list():select(3) end)
+      vim.keymap.set('n', '<leader>l', function() harpoon:list():select(4) end)
     end
   }
 }
