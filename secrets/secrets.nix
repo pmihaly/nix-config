0b7582d665739594186831777966ee536577adb7
pmihaly <misi@pappmihaly.com>
2023-12-31 16:11:53 +0100

    change ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   misi =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/9W5fVVxjEIo66iLCDfwxHh0IQ6r9R3J/Fq5b9LWNM mihaly.papp@mihalypapp-MacBook-Pro";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcOiZr3RPpmCNq5Z5YN6pWKLl5Y0FGSo6ybJ+qQ+Xeu mihaly@mihaly.codes";
   skylake =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSNGdQWXcMDIJ3LMHfHHPzgroX5QMwZI3cqAi1zExSS skylake";
   allKeys = [ misi skylake ];
