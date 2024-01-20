900df03d83aab1e6d848f9ab3d184c689ddd6de4
pmihaly <misi@pappmihaly.com>
2024-01-20 15:37:46 +0100

    add latest kernel

--- machines/skylake/default.nix
+++ machines/skylake/default.nix
@@ -45,6 +45,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
