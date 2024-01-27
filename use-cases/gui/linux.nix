531690930305115e344b2dd452a36867fa17a729
pmihaly <misi@pappmihaly.com>
2024-01-27 10:46:34 +0100

    add tuigreet

--- use-cases/gui/linux.nix
+++ use-cases/gui/linux.nix
@@ -1,4 +1,4 @@
-{ platform, lib, config, ... }:
+{ platform, pkgs, lib, config, ... }:
 
 with lib;
 let cfg = config.modules.linux;
@@ -30,6 +30,28 @@ in optionalAttrs platform.isLinux {
         rate = 48000;
       };
     };
+
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command =
+            "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+          user = "greeter";
+        };
+      };
+    };
+
+    systemd.services.greetd.serviceConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      StandardOutput = "tty";
+      StandardError = "journal";
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
+    };
+
   };
 }
 
