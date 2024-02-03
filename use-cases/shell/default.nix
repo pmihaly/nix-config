61823a89d9bcb2338132bd36ffcf4e2afae07ddf
pmihaly <misi@pappmihaly.com>
2024-02-03 06:32:18 +0100

    add persisting personaldev

--- use-cases/shell/default.nix
+++ use-cases/shell/default.nix
@@ -57,10 +57,13 @@ in {
         imports = [ ../../modules/home-manager ];
 
         modules = {
-          persistence.directories = [{
-            directory = ".ssh";
-            mode = "0700";
-          }];
+          persistence.directories = [
+            "personaldev"
+            {
+              directory = ".ssh";
+              mode = "0700";
+            }
+          ];
           shell = {
             enable = true;
             bookmarks = bookmarks // cfg.extraBookmarks;
