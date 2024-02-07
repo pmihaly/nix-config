3dc08dbda139b212b4df6619bace73d8f0c263ee
pmihaly <misi@pappmihaly.com>
2024-02-07 20:07:12 +0100

    enable duckdns

--- use-cases/server/default.nix
+++ use-cases/server/default.nix
@@ -32,7 +32,7 @@ in optionalAttrs platform.isLinux {
         endlessh.enable = true;
         monitoring.enable = true;
         hledger.enable = false;
-        duckdns.enable = false;
+        duckdns.enable = true;
         paperless.enable = true;
         syncthing.enable = true;
       };
