47e83a8d9537339e36e56fb900e67b2167df1d1a
pmihaly <misi@pappmihaly.com>
2024-06-09 07:45:14 +0200

    disable bell

--- modules/home-manager/terminal-emulator/default.nix
+++ modules/home-manager/terminal-emulator/default.nix
@@ -33,6 +33,7 @@ in
         config.enable_tab_bar = false
         config.window_close_confirmation = 'NeverPrompt'
         config.window_decorations = 'RESIZE'
+        config.audible_bell = 'Disabled'
         config.window_padding = {
           left = 150,
           right = 150,
