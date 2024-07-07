7d201cccdd2c894844c833edf2199184054583d5
pmihaly <misi@pappmihaly.com>
2024-07-07 07:19:55 +0200

    fix vscode

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -10,6 +10,20 @@ let
   cfg = config.modules.vscode;
 in
 {
+  imports = [
+    # Source: https://gist.github.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa
+    # Make vscode settings writable
+
+    (import (builtins.fetchurl {
+      url = "https://gist.githubusercontent.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa/raw/41e569ba110eb6ebbb463a6b1f5d9fe4f9e82375/mutability.nix";
+      sha256 = "4b5ca670c1ac865927e98ac5bf5c131eca46cc20abf0bd0612db955bfc979de8";
+    }) { inherit config lib; })
+
+    (import (builtins.fetchurl {
+      url = "https://gist.githubusercontent.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa/raw/41e569ba110eb6ebbb463a6b1f5d9fe4f9e82375/vscode.nix";
+      sha256 = "fed877fa1eefd94bc4806641cea87138df78a47af89c7818ac5e76ebacbd025f";
+    }) { inherit config lib pkgs; })
+  ];
   options.modules.vscode = {
     enable = mkEnableOption "vscode";
   };
@@ -19,7 +33,6 @@ in
       enableExtensionUpdateCheck = false;
       enableUpdateCheck = false;
       extensions = with pkgs.vscode-extensions; [
-        catppuccin.catppuccin-vsc
         catppuccin.catppuccin-vsc-icons
         eamodio.gitlens
         asvetliakov.vscode-neovim
@@ -59,6 +72,7 @@ in
         "zenMode.showTabs" = "none";
         "terminal.integrated.defaultProfile.osx" = "tmux";
         "terminal.integrated.defaultProfile.linux" = "tmux";
+        "extensions.ignoreRecommendations" = true;
       };
     };
   };
