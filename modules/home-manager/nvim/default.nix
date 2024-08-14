850d55fca430a7592494e75ce7729c2fb09270fa
pmihaly <misi@pappmihaly.com>
2024-08-14 09:15:31 +0200

    add vertical split to org agenda

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -51,6 +51,7 @@ in
         require('orgmode').setup({
           org_agenda_files = {'~/Sync/org/*'},
           org_default_notes_file = '~/Sync/org/refile.org',
+          win_split_mode = 'vertical';
         })
 
         require('org-bullets').setup({})
