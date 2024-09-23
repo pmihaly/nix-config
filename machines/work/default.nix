2fc8a787ec861e11cd87cf3fec0bc5d7fab798cf
pmihaly <misi@pappmihaly.com>
2024-09-23 10:24:33 +0200

    nix fmt

--- machines/work/default.nix
+++ machines/work/default.nix
@@ -16,7 +16,7 @@
     style.enable = true;
   };
 
-  ids.uids.nixbld = 300;  # i dont want to update macos
+  ids.uids.nixbld = 300; # i dont want to update macos
 
   home-manager.users.${vars.username} = {
     home.stateVersion = "22.05";
