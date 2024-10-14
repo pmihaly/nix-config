6892118549a8174bc22f31dd64f77b263a9c8e38
pmihaly <misi@pappmihaly.com>
2024-10-14 09:17:09 +0200

    altserver on linux only

--- use-cases/altserver/default.nix
+++ use-cases/altserver/default.nix
@@ -1,4 +1,5 @@
 {
+  platform,
   pkgs,
   lib,
   vars,
@@ -14,36 +15,38 @@ in
   options.modules.altserver = {
     enable = mkEnableOption "altserver";
   };
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable (
+    optionalAttrs platform.isLinux {
 
-    systemd.tmpfiles.rules =
-      let
-        directories = [
-          "${vars.storage}/Services/anisette"
+      systemd.tmpfiles.rules =
+        let
+          directories = [
+            "${vars.storage}/Services/anisette"
+          ];
+        in
+        (map (directory: "d ${directory} 0750 misi backup -") directories);
+
+      services.usbmuxd = {
+        enable = true;
+        package = pkgs.usbmuxd2;
+      };
+
+      home-manager.users.${vars.username} = {
+        home.packages = with pkgs; [
+          libimobiledevice
+          altserver-linux
         ];
-      in
-      (map (directory: "d ${directory} 0750 misi backup -") directories);
+      };
 
-    services.usbmuxd = {
-      enable = true;
-      package = pkgs.usbmuxd2;
-    };
-
-    home-manager.users.${vars.username} = {
-      home.packages = with pkgs; [
-        libimobiledevice
-        altserver-linux
-      ];
-    };
-
-    virtualisation.oci-containers.containers.alt_anisette_server = {
-      image = "dadoum/anisette-v3-server";
-      ports = [
-        "6969:6969"
-      ];
-      volumes = [
-        "${vars.storage}/Services/anisette:/home/Alcoholic/.config/anisette-v3/lib/"
-      ];
-    };
-  };
+      virtualisation.oci-containers.containers.alt_anisette_server = {
+        image = "dadoum/anisette-v3-server";
+        ports = [
+          "6969:6969"
+        ];
+        volumes = [
+          "${vars.storage}/Services/anisette:/home/Alcoholic/.config/anisette-v3/lib/"
+        ];
+      };
+    }
+  );
 }
