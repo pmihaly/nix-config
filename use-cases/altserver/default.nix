911c5516bda8db565cf03fea046dc28a79bebc48
pmihaly <misi@pappmihaly.com>
2024-10-18 18:04:04 +0200

    altserver with wifi

--- use-cases/altserver/default.nix
+++ use-cases/altserver/default.nix
@@ -10,6 +10,33 @@
 with lib;
 let
   cfg = config.modules.altserver;
+  netmuxd-src = pkgs.fetchFromGitHub {
+    owner = "jkcoxson";
+    repo = "netmuxd";
+    rev = "7e6c520975e8d34045e36f9d58f9f529285337f5";
+    hash = "sha256-NLWo+cAW8oBDaX+YjVb/gOgL/vw7P5ys3kMEX00OI/A=";
+  };
+  netmuxd = pkgs.rustPlatform.buildRustPackage {
+    pname = "netmuxd";
+    version = "0.1";
+    cargoLock = {
+      lockFile = netmuxd-src + /Cargo.lock;
+      allowBuiltinFetchGit = true;
+    };
+    src = netmuxd-src;
+    buildNoDefaultFeatures = true;
+    buildFeatures = [ "usb" ];
+
+    buildInputs = with pkgs; [
+      openssl
+      libimobiledevice
+    ];
+
+    nativeBuildInputs = with pkgs; [
+      perl
+      pkg-config
+    ];
+  };
 in
 {
   options.modules.altserver = {
@@ -28,7 +55,6 @@ in
 
       services.usbmuxd = {
         enable = true;
-        package = pkgs.usbmuxd2;
       };
 
       home-manager.users.${vars.username} = {
@@ -36,6 +62,47 @@ in
           libimobiledevice
           altserver-linux
         ];
+      };
+
+      programs.zsh.shellAliases = {
+        netmuxd = "${netmuxd}/bin/netmuxd --disable-unix --host 127.0.0.1";
+        altserver = "USBMUXD_SOCKET_ADDRESS='127.0.0.1:27015' ALTSERVER_ANISETTE_SERVER='http://127.0.0.1:6969' ${getExe pkgs.altserver-linux} -a '***@icloud.com' -p '***'";
+      };
+
+      systemd.services = {
+        netmuxd = {
+          wantedBy = [ "multi-user.target" ];
+          requires = [ "usbmuxd.service" ];
+          after = [ "network.target" ];
+          description = "";
+
+          serviceConfig = {
+            Restart = "always";
+            ExecStart = config.programs.zsh.shellAliases.netmuxd;
+          };
+        };
+
+        altserver-server = {
+          wantedBy = [ "multi-user.target" ];
+          requires = [ "netmuxd.service" "usbmuxd.service" ];
+          after = [ "network.target" ];
+          description = "";
+
+          serviceConfig = {
+            Restart = "always";
+            ExecStart = config.programs.zsh.shellAliases.altserver;
+          };
+        };
+      };
+
+      services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+        nssmdns6 = true;
+        publish = {
+          enable = true;
+          userServices = true;
+        };
       };
 
       virtualisation.oci-containers.containers.alt_anisette_server = {
