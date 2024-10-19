88a9780d1a2ff5b78988034d6029721941a74642
pmihaly <misi@pappmihaly.com>
2024-10-19 06:08:30 +0200

    windows based altserver

--- use-cases/altserver/default.nix
+++ use-cases/altserver/default.nix
@@ -10,33 +10,6 @@
 with lib;
 let
   cfg = config.modules.altserver;
-  netmuxd-src = pkgs.fetchFromGitHub {
-    owner = "jkcoxson";
-    repo = "netmuxd";
-    rev = "7e6c520975e8d34045e36f9d58f9f529285337f5";
-    hash = "sha256-NLWo+cAW8oBDaX+YjVb/gOgL/vw7P5ys3kMEX00OI/A=";
-  };
-  netmuxd = pkgs.rustPlatform.buildRustPackage {
-    pname = "netmuxd";
-    version = "0.1";
-    cargoLock = {
-      lockFile = netmuxd-src + /Cargo.lock;
-      allowBuiltinFetchGit = true;
-    };
-    src = netmuxd-src;
-    buildNoDefaultFeatures = true;
-    buildFeatures = [ "usb" ];
-
-    buildInputs = with pkgs; [
-      openssl
-      libimobiledevice
-    ];
-
-    nativeBuildInputs = with pkgs; [
-      perl
-      pkg-config
-    ];
-  };
 in
 {
   options.modules.altserver = {
@@ -45,73 +18,39 @@ in
   config = mkIf cfg.enable (
     optionalAttrs platform.isLinux {
 
-      systemd.tmpfiles.rules =
-        let
-          directories = [
-            "${vars.storage}/Services/anisette"
-          ];
-        in
-        (map (directory: "d ${directory} 0750 misi backup -") directories);
+      systemd.tmpfiles.rules = [
+        "d ${vars.storage}/Services/altserver-windows 0755 ${vars.username} multimedia - -"
+      ];
 
-      services.usbmuxd = {
-        enable = true;
-      };
-
-      home-manager.users.${vars.username} = {
-        home.packages = with pkgs; [
-          libimobiledevice
-          altserver-linux
+      networking.firewall = {
+        allowedTCPPorts = [
+          8006
+          3389
         ];
+        allowedUDPPorts = [ 3389 ];
       };
 
-      programs.zsh.shellAliases = {
-        netmuxd = "${netmuxd}/bin/netmuxd --disable-unix --host 127.0.0.1";
-        altserver = "USBMUXD_SOCKET_ADDRESS='127.0.0.1:27015' ALTSERVER_ANISETTE_SERVER='http://127.0.0.1:6969' ${getExe pkgs.altserver-linux} -a '***@icloud.com' -p '***'";
-      };
-
-      systemd.services = {
-        netmuxd = {
-          wantedBy = [ "multi-user.target" ];
-          requires = [ "usbmuxd.service" ];
-          after = [ "network.target" ];
-          description = "";
-
-          serviceConfig = {
-            Restart = "always";
-            ExecStart = config.programs.zsh.shellAliases.netmuxd;
-          };
+      virtualisation.oci-containers.containers.altserver-windows = {
+        image = "dockurr/windows";
+        environment = {
+          VERSION = "win11";
+          USERNAME = "altserver";
+          PASSWORD = "altserver";
+          ARGUMENTS = "-device usb-host,vendorid=0x05ac,productid=0x12a8"; # should be working for any iphone
         };
-
-        altserver-server = {
-          wantedBy = [ "multi-user.target" ];
-          requires = [ "netmuxd.service" "usbmuxd.service" ];
-          after = [ "network.target" ];
-          description = "";
-
-          serviceConfig = {
-            Restart = "always";
-            ExecStart = config.programs.zsh.shellAliases.altserver;
-          };
-        };
-      };
-
-      services.avahi = {
-        enable = true;
-        nssmdns4 = true;
-        nssmdns6 = true;
-        publish = {
-          enable = true;
-          userServices = true;
-        };
-      };
-
-      virtualisation.oci-containers.containers.alt_anisette_server = {
-        image = "dadoum/anisette-v3-server";
         ports = [
-          "6969:6969"
+          "8006:8006"
+          "3389:3389/tcp"
+          "3389:3389/udp"
         ];
         volumes = [
-          "${vars.storage}/Services/anisette:/home/Alcoholic/.config/anisette-v3/lib/"
+          "${vars.storage}/Services/altserver-windows:/storage"
+        ];
+        extraOptions = [
+          "--device=/dev/kvm"
+          "--device=/dev/bus/usb"
+          "--device=/dev/net/tun"
+          "--cap-add=all"
         ];
       };
     }
