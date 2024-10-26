f9a73949a40091682517256fb58cdd8f0fd4a681
pmihaly <misi@pappmihaly.com>
2024-10-26 07:25:30 +0200

    altserver dashboard

--- use-cases/altserver/default.nix
+++ use-cases/altserver/default.nix
@@ -1,6 +1,5 @@
 {
   platform,
-  pkgs,
   lib,
   vars,
   config,
@@ -16,43 +15,51 @@ in
     enable = mkEnableOption "altserver";
   };
   config = mkIf cfg.enable (
-    optionalAttrs platform.isLinux {
-
-      systemd.tmpfiles.rules = [
-        "d ${vars.storage}/Services/altserver-windows 0755 ${vars.username} multimedia - -"
-      ];
-
-      networking.firewall = {
-        allowedTCPPorts = [
-          8006
-          3389
-        ];
-        allowedUDPPorts = [ 3389 ];
+    optionalAttrs platform.isLinux (mkService {
+      subdomain = "altserver-windows";
+      port = 8006;
+      dashboard = {
+        category = "Misc";
+        name = "Altserver";
+        logo = ./windows.svg;
       };
+      extraConfig = {
+        systemd.tmpfiles.rules = [
+          "d ${vars.storage}/Services/altserver-windows 0755 ${vars.username} multimedia - -"
+        ];
 
-      virtualisation.oci-containers.containers.altserver-windows = {
-        image = "dockurr/windows";
-        environment = {
-          VERSION = "win11";
-          USERNAME = "altserver";
-          PASSWORD = "altserver";
-          ARGUMENTS = "-device usb-host,vendorid=0x05ac,productid=0x12a8"; # should be working for any iphone
+        networking.firewall = {
+          allowedTCPPorts = [
+            8006
+            3389
+          ];
+          allowedUDPPorts = [ 3389 ];
         };
-        ports = [
-          "8006:8006"
-          "3389:3389/tcp"
-          "3389:3389/udp"
-        ];
-        volumes = [
-          "${vars.storage}/Services/altserver-windows:/storage"
-        ];
-        extraOptions = [
-          "--device=/dev/kvm"
-          "--device=/dev/bus/usb"
-          "--device=/dev/net/tun"
-          "--cap-add=all"
-        ];
+
+        virtualisation.oci-containers.containers.altserver-windows = {
+          image = "dockurr/windows";
+          environment = {
+            VERSION = "win11";
+            USERNAME = "altserver";
+            PASSWORD = "altserver";
+            ARGUMENTS = "-device usb-host,vendorid=0x05ac,productid=0x12a8"; # should be working for any iphone
+          };
+          ports = [
+            "8006:8006"
+            "3389:3389/tcp"
+            "3389:3389/udp"
+          ];
+          volumes = [
+            "${vars.storage}/Services/altserver-windows:/storage"
+          ];
+          extraOptions = [
+            "--device=/dev/kvm"
+            "--device=/dev/bus/usb"
+            "--device=/dev/net/tun"
+            "--cap-add=all"
+          ];
+        };
       };
-    }
+    })
   );
 }
