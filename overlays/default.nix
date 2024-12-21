aac21ae889fb3bce0594b782bd1504408d388ee9
pmihaly <misi@pappmihaly.com>
2024-12-21 07:15:03 +0100

    remove iosveka custom

--- overlays/default.nix
+++ overlays/default.nix
@@ -27,49 +27,49 @@
 
     iosevka-custom = prev.iosevka.override {
       privateBuildPlan = ''
-        [buildPlans.iosevka-custom]
+        [buildPlans.Iosevka-custom]
         family = "Iosevka Custom"
         spacing = "fontconfig-mono"
         serifs = "slab"
-        no-cv-ss = true
-        export-glyph-names = true
+        noCvSs = true
+        exportGlyphNames = true
 
-          [buildPlans.iosevka-custom.ligations]
+          [buildPlans.Iosevka-custom.ligations]
           inherits = "dlig"
 
-        [buildPlans.iosevka-custom.weights.regular]
+        [buildPlans.Iosevka-custom.weights.regular]
         shape = 400
         menu = 400
         css = 400
 
-        [buildPlans.iosevka-custom.weights.bold]
+        [buildPlans.Iosevka-custom.weights.bold]
         shape = 700
         menu = 700
         css = 700
 
-        [buildPlans.iosevka-custom.weights.extrabold]
+        [buildPlans.Iosevka-custom.weights.extrabold]
         shape = 800
         menu = 800
         css = 800
 
-        [buildPlans.iosevka-custom.weights.heavy]
+        [buildPlans.Iosevka-custom.weights.heavy]
         shape = 900
         menu = 900
         css = 900
 
-        [buildPlans.iosevka-custom.slopes.upright]
+        [buildPlans.Iosevka-custom.slopes.upright]
         angle = 0
         shape = "upright"
         menu = "upright"
         css = "normal"
 
-        [buildPlans.iosevka-custom.slopes.italic]
+        [buildPlans.Iosevka-custom.slopes.italic]
         angle = 9.4
         shape = "italic"
         menu = "italic"
         css = "italic"
       '';
-      set = "custom";
+      set = "-custom";
     };
 
     keepassxc =
