d70ba4aef818e3910efad1844c7f07dbe7d57698
pmihaly <misi@pappmihaly.com>
2024-12-25 17:42:50 +0100

    Revert "add hyprland dynamic cursors"
    This reverts commit 6f107005a565b2bfec52a0b15c65295150048a9f.

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -50,10 +50,6 @@ in
       xwayland.enable = true;
       sourceFirst = true;
 
-      plugins = with pkgs.hyprlandPlugins; [
-        hypr-dynamic-cursors
-      ];
-
       settings = {
         monitor = [
           "DP-1, 2560x1440@144, 0x0, 1"
@@ -197,11 +193,6 @@ in
           "$mainMod, mouse:272, movewindow"
           "$mainMod, mouse:273, resizewindow"
         ];
-
-        "plugin:dynamic-cursors" = {
-          enabled = true;
-          mode = "stretch";
-        };
       };
     };
 
