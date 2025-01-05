178f37b03f6d70aca53dd1686a90ba8b83d47d2d
pmihaly <misi@pappmihaly.com>
2025-01-05 14:25:44 +0100

    dont run ns in loop

--- machines/work/vars.nix
+++ machines/work/vars.nix
@@ -1,4 +1,4 @@
 {
   username = "mihaly.papp";
-  rebuildSwitch = "while true; do nh darwin switch ~/.nix-config --hostname mac -- --impure --option extra-experimental-features pipe-operators --impure && break; done";
+  rebuildSwitch = "nh darwin switch ~/.nix-config --hostname mac -- --impure --option extra-experimental-features pipe-operators --impure";
 }
