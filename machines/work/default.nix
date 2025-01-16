14af47b18263937e0b7e36360935c38694bec861
pmihaly <misi@pappmihaly.com>
2025-01-16 07:08:33 +0100

    do not open graylog

--- machines/work/default.nix
+++ machines/work/default.nix
@@ -118,7 +118,7 @@ in
       mr = "open \"https://gitlab.${workvars.domain}/lensa/phoenix/$(basename \"$(pwd)\")/-/merge_requests?scope=all&state=opened&search=$(ticket)\"";
       devenv = "set -o allexport && source config/dev.env";
       so = "source ./.ve/bin/activate ; devenv";
-      gl = "mkdir -p ~/tun ; pushd ~/tun ; ssh -T graylog-staging-tunnel & (sleep 4 ; open http://localhost:9988)";
+      gl = "mkdir -p ~/tun ; pushd ~/tun ; ssh -T graylog-staging-tunnel";
       db = "mkdir -p ~/db ; pushd ~/db ; nvim -c DBUI";
     };
 
