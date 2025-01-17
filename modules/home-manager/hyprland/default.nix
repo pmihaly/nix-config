3a89bad15eec25dd38f0a438c4e9a94c872d4ab2
pmihaly <misi@pappmihaly.com>
2025-01-17 17:28:32 +0100

    disable hyprland update news

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -51,7 +51,7 @@ in
       sourceFirst = true;
 
       settings = {
-        debug.disable_logs = false;
+        ecosystem.no_update_news = true;
         monitor = [
           "DP-1, 2560x1440@144, 0x0, 1"
           "HDMI-A-1, 1920x1080, 2560x0, 1"
