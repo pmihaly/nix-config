57470315029e424a1c2cdb770399d6f6bd67ac28
pmihaly <misi@pappmihaly.com>
2025-03-20 15:19:58 +0100

    firefox disable password saving

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -223,6 +223,7 @@ in
             '';
             extraConfig = ''
               user_pref("browser.translation.enable", "false");
+              user_pref("signon.rememberSignons", "false");
 
               user_pref("app.normandy.api_url", "");
               user_pref("app.normandy.enabled", false);
