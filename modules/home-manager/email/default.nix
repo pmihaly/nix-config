04cf40c14479c83b2e676522691c5b1a831ecc51
pmihaly <misi@pappmihaly.com>
2025-03-29 13:25:26 +0100

    remove maildir

--- modules/home-manager/email/default.nix
+++ modules/home-manager/email/default.nix
@@ -15,8 +15,6 @@ in
   imports = [ ../../../secrets/home-manager ];
   config = mkIf cfg.enable {
 
-    modules.persistence.directories = [ "Maildir" ];
-
     accounts.email.accounts = {
       "mihaly_mihaly.codes" = rec {
         address = "mihaly@mihaly.codes";
