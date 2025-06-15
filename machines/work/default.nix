cadfbc7fd48897624a80b98a4702cf69e17f5d9f
pmihaly <misi@pappmihaly.com>
2025-06-15 09:12:49 +0200

    reset work

--- machines/work/default.nix
+++ machines/work/default.nix
@@ -10,11 +10,6 @@ let
     builtins.readFile "/Users/${vars.username}/.nix-config/machines/work/workvars.toml"
   );
   envs = [
-    "demo-px"
-    "demo-al"
-    "demo-vr"
-    "demo-am2"
-    "demo-fp2"
   ];
 in
 rec {
@@ -27,7 +22,7 @@ rec {
       enable = true;
       extraBookmarks = {
         su = "'~/Library/Application Support'";
-        w = "~/lensadev";
+        w = "~/work";
       };
     };
 
@@ -51,24 +46,8 @@ rec {
       bookmarks.settings =
         [
           {
-            name = "jumpcloud";
-            url = "https://console.jumpcloud.com";
-          }
-          {
             name = "jira";
             url = "${workvars.jira-url}/PHOENIX";
-          }
-          {
-            name = "datadog";
-            url = "https://app.datadoghq.com/dashboard";
-          }
-          {
-            name = "prod graylog";
-            url = "https://graylog.${workvars.domain}";
-          }
-          {
-            name = "local graylog";
-            url = "localhost:9988";
           }
           {
             name = "gmail";
@@ -96,99 +75,17 @@ rec {
               }
             ])
             [
-              "career-and-social-infra"
-              "career-and-social-sites"
-              "career-assistant"
-              "company-and-job-title"
-              "company-and-job-title-moderation"
-              "company-match"
-              "phoenix-overview"
-              "scripts"
-              "social-feed-engine"
-              "video-catalog"
-              "video-director"
-              "video-editor"
-              "video-infra"
-              "video-player"
-              "video-streamer"
-              "kokoro-fastapi"
             ];
     };
 
-    programs.lazygit.settings.services = {
-      "gsh.${workvars.domain}" = "gitlab:gitlab.${workvars.domain}";
-    };
-
-    home.packages =
-      with pkgs;
-      [
-        awscli
-        git-lfs
-        saml2aws
-        openssl
-        obsidian
-        jwt-cli
-        slack
-        gnumake
-        mariadb # vim dadbod
-        jetbrains.pycharm-community-bin
-        aegisub
-        (pkgs.writers.writePython3Bin "json-to-pq" { libraries = [ pkgs.python3Packages.polars ]; } ''
-          import sys
-          import polars as pl
-          from io import BytesIO
-
-          df = pl.read_json(sys.stdin.buffer.read())
-
-          buf = BytesIO()
-          df.write_parquet(buf)
-          sys.stdout.buffer.write(buf.getvalue())
-        '')
-        (pkgs.writers.writePython3Bin "pq-to-json" { libraries = [ pkgs.python3Packages.polars ]; } ''
-          import sys
-          import polars as pl
-          from io import BytesIO
-
-          df = pl.read_parquet(sys.stdin.buffer.read())
-
-          buf = BytesIO()
-          df.write_json(buf)
-          sys.stdout.buffer.write(buf.getvalue())
-        '')
-      ]
-      ++ (concatMap (
-        env:
-        let
-          envId = env |> splitString "-" |> lists.last;
-        in
-        [
-          (pkgs.writeShellScriptBin "trget-${envId}" "${getExe' pkgs.httpie "http"} get http://tracing-system-app.${env}.lms/api/v1/business-process/$1")
-          (pkgs.writeShellScriptBin "trparent-${envId}" "${getExe pkgs.jq} -r .parent_global_trace_id | xargs -I{} ${getExe' pkgs.httpie "http"} get http://tracing-system-app.${env}.lms/api/v1/business-process/{}")
-          (pkgs.writeShellScriptBin "trcreate-${envId}" ''${getExe pkgs.jq} '{"source": "misi_lol", "global_state": .}' | ${getExe' pkgs.httpie "http"} post http://tracing-system-app.${env}.lms/api/v1/business-process/$1/v1 | ${getExe pkgs.jq} '.global_trace_id' -r | xargs -I{} trget-${envId} {}'')
-          (pkgs.writeScriptBin "dlvid-${envId}" ''
-            #! ${getExe pkgs.nushell}
-            def main [
-              vid: string # video id
-            ]: nothing -> bool {
-              {filters: [{video_id: $vid}]}
-              | to json
-              | http post http://video-streamer.${env}.lms/v1/get-video-playback-details
-              | get ([$vid, "playback_url"] | into cell-path)
-              | ${getExe pkgs.yt-dlp} $in -o $"($vid).mp4"
-              }
-          '')
-          (pkgs.writeShellScriptBin "dump-${envId}" "${getExe' pkgs.mariadb "mysqldump"} --user=${workvars.demo-db-user} --password=${workvars.demo-db-password} --host=${env}-mysql8.demo --port=3307 $1")
-        ]
-      ) envs);
+    home.packages = with pkgs; [
+      slack
+      mariadb # vim dadbod
+    ];
 
     programs.nushell.extraConfig = ''
       def --env d [] { ls ${modules.shell.extraBookmarks.w} | where type == "dir" | par-each {{path: $in.name, name: ($in.name | split row "/" | last)}} | input list --fuzzy --display name | cd $in.path }
       def --env dn [] { p ; nvim }
-
-      def "from pq" [] { pq-to-json | from json }
-      def "to pq" [] { to json | json-to-pq }
-
-      def unlockpdf [password: string, filename: string] { ${getExe pkgs.qpdf} --password=$password --decrypt $filename $"unlocked_($filename)" }
 
       def ticket [] { git branch --show-current | split words | take 2 | str join "-" }
       def jira [] { ticket | start $"${workvars.jira-url}/($in)" }
@@ -198,25 +95,11 @@ rec {
     '';
 
     programs.nixvim = {
-      keymaps = [
-        {
-          mode = [
-            "n"
-          ];
-          key = "<leader>mf";
-          action = ":make flint<cr>";
-        }
-      ];
-
       globals.dbs =
         [
           {
             name = "local";
             url = "mysql://root@127.0.0.1:3306";
-          }
-          {
-            name = "staging (tun)";
-            url = workvars.staging-mysql-url;
           }
         ]
         ++ map (env: {
@@ -224,66 +107,12 @@ rec {
           url = "mysql://${workvars.demo-db-user}:${workvars.demo-db-password}@${env}-mysql8.demo:3307";
         }) envs;
     };
-
-    programs.ssh = {
-      enable = true;
-      addKeysToAgent = "yes";
-      matchBlocks = {
-        "*" = {
-          identityFile = "~/.ssh/lensa2";
-        };
-
-        "gitlab.${workvars.domain}" = {
-          host = workvars.domain;
-          identityFile = "~/.ssh/lensa2";
-        };
-
-        natstaging = {
-          hostname = workvars.ips.staging;
-          identityFile = "~/.ssh/lensa2";
-        };
-
-        "${workvars.ips.natstaging-range}" = {
-          identityFile = "~/.ssh/new-staging2018.pem";
-          user = workvars.natstaging-user;
-          identitiesOnly = true;
-          proxyCommand = "ssh natstaging -W %h:%p";
-        };
-
-        graylog-staging-tunnel = {
-          user = "mihaly.papp";
-          hostname = workvars.ips.staging;
-          identityFile = "~/.ssh/lensa2";
-          localForwards = [
-            {
-              bind.port = 9988;
-              host.address = workvars.ips.staging-graylog;
-              host.port = 9000;
-            }
-          ];
-        };
-
-        demo-px = {
-          user = workvars.natstaging-user;
-          hostname = workvars.ips.demo-px;
-          identitiesOnly = true;
-          identityFile = "~/.ssh/new-staging2018.pem";
-          proxyCommand = "ssh natstaging -W %h:%p";
-          setEnv = {
-            TERM = "xterm";
-          };
-
-        };
-      };
-
-    };
   };
 
   homebrew.casks = [
     "docker"
     "flux"
     "google-chrome"
-    "fontlab"
   ];
 
   users.users.${vars.username}.home = "/Users/${vars.username}";
