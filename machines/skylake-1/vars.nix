9799903442c8900b6a1cf27411ea49de197d87c2
pmihaly <misi@pappmihaly.com>
2025-09-20 13:13:46 +0200

    rename skylake-1 to skylake

--- machines/skylake-1/vars.nix
+++
@@ -1,9 +0,0 @@
-rec {
-  domainName = "skylake-1.anaconda-snapper.ts.net";
-  timeZone = "Europe/Helsinki";
-  persistDir = "/persist";
-  serviceConfig = "${persistDir}/opt/skylake-services";
-  storage = "${persistDir}/opt/skylake-storage";
-  username = "misi";
-  rebuildSwitch = "nh os switch ~/.nix-config --hostname skylake-1";
-}
