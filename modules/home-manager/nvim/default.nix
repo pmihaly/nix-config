3ec877c222cc2a152d0455be43ba138443969d93
pmihaly <misi@pappmihaly.com>
2025-10-30 19:24:48 +0100

    add lsps

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -20,6 +20,37 @@ in
     ];
     programs.bash.sessionVariables.EDITOR = getExe config.programs.nixvim.build.package;
 
-    programs.nixvim.enable = true;
+    programs.nixvim = {
+      enable = true;
+      plugins = {
+        lsp = {
+          enable = true;
+          servers = {
+            basedpyright.enable = true;
+            nixd.enable = true;
+            gopls.enable = true;
+          };
+        };
+        cmp = {
+          enable = true;
+          settings = {
+            mapping = {
+              "<C-Space>" = "cmp.mapping.complete()";
+              "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+              "<C-e>" = "cmp.mapping.close()";
+              "<C-f>" = "cmp.mapping.scroll_docs(4)";
+              "<CR>" = "cmp.mapping.confirm({ select = true })";
+              "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+              "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            };
+            sources = [
+              { name = "nvim_lsp"; }
+              { name = "nvim_lsp_signature_help"; }
+              { name = "buffer"; }
+            ];
+          };
+        };
+      };
+    };
   };
 }
