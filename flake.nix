8af3bd594268e1a4b80a8857b4903372119ceaaf
pmihaly <misi@pappmihaly.com>
2026-08-24 07:37:27 +0200

    remove comfyui

--- flake.nix
+++ flake.nix
@@ -82,11 +82,6 @@
       url = "github:musnix/musnix";
     };
     nixpkgs-working-elektroid.url = "github:nixos/nixpkgs/23b3807ea8241d5f73e5161c1dd706966108c5b9";
-    comfyui = {
-      # inputs.nixpkgs.follows = "nixpkgs";
-      # url = "github:utensils/comfyui-nix?ref=fix/rocm-xformers-segfault";
-      url = "github:utensils/comfyui-nix";
-    };
     boxes = {
       url = "github:pmihaly/boxes";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -231,21 +226,10 @@
           inputs.stylix.nixosModules.stylix
           inputs.copyparty.nixosModules.default
           inputs.musnix.nixosModules.musnix
-          # inputs.comfyui.nixosModules.default
           {
             nixpkgs.overlays = [
               inputs.nur.overlays.default
               inputs.copyparty.overlays.default
-              inputs.comfyui.overlays.default
-              (final: prev: {
-                python312 = prev.python312.override {
-                  packageOverrides = pyFinal: pyPrev: {
-                    comfyui-manager = pyPrev.comfyui-manager.overrideAttrs (_: {
-                      dontCheckRuntimeDeps = true;
-                    });
-                  };
-                };
-              })
             ];
           }
           {
