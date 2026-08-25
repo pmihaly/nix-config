22b9ab9a37f81973a0de32ab83205f612a5922fe
Mihaly Papp <mihaly.papp@fareharbor.com>
2026-08-25 08:49:02 +0200

    add ssh key

--- machines/work/default.nix
+++ machines/work/default.nix
@@ -57,6 +57,14 @@ in
     ::1 localhost
    '';
 
+  programs.ssh.knownHosts."github.com" = {
+    hostNames = [
+      "github.com"
+      "ssh.github.com"
+    ];
+    publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+  };
+
   home-manager.backupFileExtension = "backup";
   home-manager.overwriteBackup = true;
   home-manager.users.${vars.username} = {
